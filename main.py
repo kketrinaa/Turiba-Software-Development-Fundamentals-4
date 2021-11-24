a0f8cec1a10fb022e941a7b4673b0aa053120748
kketrinaa <84866469+kketrinaa@users.noreply.github.com>
2021-11-24 18:26:34 +0200

    Update main.py

--- main.py
+++ main.py
@@ -24,10 +24,10 @@ while True:
         num2 = int(input("Enter second number: "))
 
         if choice == '1':
-            print(num1, "+", num2, "=", add(num1, num2))
+            print(add(num1, num2))
 
         elif choice == '2':
-            print(num1, "-", num2, "=", subtract(num1, num2))
+            print(subtract(num1, num2))
 
         elif choice == '3':
-            print(num1, "*", num2, "=", multiply(num1, num2))
+            print(multiply(num1, num2))
